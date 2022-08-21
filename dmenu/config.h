f1da85f2d54c2b42ee418050a585ecb14eae0169
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-08-21 11:54:05 +0800

    update config dmenu

--- dmenu/config.h
+++ dmenu/config.h
@@ -13,6 +13,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	/* [SchemeSel] = { "#eeeeee", "#e60012" }, */ // default #005577
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
