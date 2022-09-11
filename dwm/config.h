1ad18b46c960aa4f8d643f10658fd058928212cd
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-09-11 12:34:18 +0800

    update config.h focusnthmon

--- dwm/config.h
+++ dwm/config.h
@@ -82,9 +82,9 @@ static const Layout layouts[] = {
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} }, 
-        /* { ALTMOD,                       KEY,      focusnthmon,    {.i  = TAG } }, \ */
-        /* { ALTMOD|ShiftMask,             KEY,      tagnthmon,      {.i  = TAG } }, */
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} }, \
+        { ALTMOD,                       KEY,      focusnthmon,    {.i  = TAG } }, \
+        { ALTMOD|ShiftMask,             KEY,      tagnthmon,      {.i  = TAG } },
 
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
