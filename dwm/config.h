1d2b812887bccc23c02453651ed8501b68c17fa6
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-09-12 15:47:55 +0800

    centered and floating telegram, whatsapp, firefox, add discord to window 2

--- dwm/config.h
+++ dwm/config.h
@@ -48,13 +48,15 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class                instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "TelegramDesktop",    NULL,     NULL,           1 << 1,         0,          0,           0,        -1 },
-	{ "whatsdesk",          NULL,     NULL,           1 << 1,         0,          0,           0,        -1 },
+	{ "TelegramDesktop",    NULL,     NULL,           1 << 1,         1,          0,           0,        -1 },
+	{ "discord",            NULL,     NULL,           1 << 1,         1,          0,           0,        -1 },
+	{ "whatsdesk",          NULL,     NULL,           1 << 1,         1,          0,           0,        -1 },
 	{ "obs",                NULL,     NULL,           0,              1,          0,           0,        -1 },
 	{ "Lutris",             NULL,     NULL,           0,              1,          0,           0,        -1 },
-	{ "firefox",   		    NULL,     NULL,           1 << 2,         0,          0,          -1,        -1 },
+	{ "firefox",   		    NULL,     NULL,           1 << 2,         1,          0,          -1,        -1 },
 	{ "qutebrowser",   		NULL,     NULL,           1 << 2,         0,          0,          -1,        -1 },
 	{ "St",                 NULL,     NULL,           0,              1,          1,           0,        -1 },
+	{ "gimp",               NULL,     NULL,           1 << 3,         1,          0,           0,        -1 },
 	{ NULL,                 NULL,     "Event Tester", 0,              0,          0,           1,        -1 }, /* xev */
 };
 
