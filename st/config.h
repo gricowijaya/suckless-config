83d41afd5af35ad4303f1419fcd943598c21de34
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-09-22 09:53:51 +0800

    update font size in st

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "TerminessTTF Nerd Font Mono:pixelsize=19:antialias=true:autohint=true";
+static char *font = "TerminessTTF Nerd Font Mono:pixelsize=21:antialias=true:autohint=true";
 static int borderpx = 25;
 
 /*
