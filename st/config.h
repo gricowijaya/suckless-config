319c245e21f2f7d20ae57ca6682b12190bf061be
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-09-26 15:33:36 +0800

    update mode of st

--- st/config.h
+++ st/config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "TerminessTTF Nerd Font Mono:pixelsize=21:antialias=true:autohint=true";
-static int borderpx = 25;
+static char *font = "Terminus (TTF):pixelsize=20:antialias=true:autohint=true";
+static int borderpx = 20;
 
 /*
  * What program is execed by st depends of these precedence rules:
