4235871e62d03a5eb0f241328148e1abc7d342db
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-09-27 14:43:16 +0800

    update for postman and chromium and discord instance and tags mask

--- dwm/config.h
+++ dwm/config.h
@@ -55,10 +55,13 @@ static const Rule rules[] = {
 	 */
 	/* class                instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "TelegramDesktop",    NULL,     NULL,           1 << 6,         0,          0,           0,        -1 },
+	{ "discord",            NULL,     NULL,           1 << 7,         0,          0,           0,        -1 },
 	{ "zoom",               NULL,     NULL,           1 << 5,         0,          0,           0,        -1 },
 	{ "obs",                NULL,     NULL,           1 << 4,         0,          0,           0,        -1 },
 	{ "firefox",   		    NULL,     NULL,           1 << 2,         0,          0,          -1,        -1 },
 	{ "qutebrowser",   		NULL,     NULL,           1 << 2,         0,          0,          -1,        -1 },
+	{ "chromium",   		NULL,     NULL,           1 << 8,         0,          0,          -1,        -1 },
+	{ "postman",   		    NULL,     NULL,           1 << 1,         0,          0,          -1,        -1 },
 	{ "St",                 NULL,     NULL,           0,              0,          1,           0,        -1 },
 	{ NULL,                 NULL,     "Event Tester", 0,              0,          0,           1,        -1 }, /* xev */
 };
