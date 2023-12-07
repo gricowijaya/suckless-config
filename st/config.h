2fc189e478bfe0b7bedb9ea59d11dbe1964e8278
Gede Rico Wijaya <gricowijaya@gmail.com>
2023-12-07 15:36:00 +0800

    change default font

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "VictorMono Nerd Font Mono:pixelsize=18:antialias=true:autohint=true";
+static char *font = "monospace:pixelsize=23:antialias=true:autohint=true";
 static int borderpx = 10;
 
 /*
