46a02c3deaa9fd5fac954c75c89052b75d3a6642
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-01-12 23:45:35 +0800

    disable topbar and extrabar
    to activate the topbar use `Super` + `g`
    
    to activate the extrabar user `Super` + `Shift` + `g`

--- dwm/config.h
+++ dwm/config.h
@@ -17,9 +17,9 @@ static const unsigned int gappov         = 10;       /* vert outer gap between w
 static       int smartgaps               = 0;        /* 1 means no outer gap when there is only one window */
 static const unsigned int snap           = 32;       /* snap pixel */
 static const int swallowfloating         = 0;        /* 1 means swallow floating windows by default */
-static const int showbar                 = 1;        /* 0 means no bar */
+static const int showbar                 = 0;        /* 0 means no bar */
 static const int topbar                  = 1;        /* 0 means no top bar */
-static const int extrabar                = 1;        /* 0 means no bottom bar */
+static const int extrabar                = 0;        /* 0 means no bottom bar */
 static const unsigned int systraypinning = 0;        /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft  = 0;        /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;        /* systray spacing */
@@ -162,6 +162,8 @@ static Key keys[] = {
 	{ 0,                            XK_F3,     spawn,          {.v = upvol   } },
 	{ 0,                            XK_F5,     spawn,          {.v = downbright } },
 	{ 0,                            XK_F6,     spawn,          {.v = upbright } },
+	{ MODKEY,                       XK_g,                         togglebar,            {0} },
+	{ MODKEY|ShiftMask,             XK_g,                         toggleextrabar,       {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
