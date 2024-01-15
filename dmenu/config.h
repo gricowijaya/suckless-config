0632b0cfae4d2d48794b7c085827168cc6a6747a
Gede Rico Wijaya <gricowijaya@gmail.com>
2024-01-15 21:43:48 +0800

    use terminus font for dmenu and st

--- dmenu/config.h
+++ dmenu/config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Ubuntu NF:size=12",
+	"Terminus:size=12",
 	"JoyPixels:pixelsize=12:antialias=true:autohint=true"
 };
 static const unsigned int bgalpha = 0xe0;
